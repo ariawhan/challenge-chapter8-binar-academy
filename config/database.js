fb11ca21578ee814696cbf09e9e90510ed97807c
Ariawhan <gede.aryawhan@gmail.com>
2022-06-02 22:28:03 +0800

    [Ariawan] Update database.js remove _developmen

--- config/database.js
+++ config/database.js
@@ -10,7 +10,7 @@ module.exports = {
   development: {
     username: DB_USER,
     password: DB_PASSWORD,
-    database: `${DB_NAME}_development`,
+    database: `${DB_NAME}`,
     host: DB_HOST,
     port: DB_PORT,
     dialect: "postgres"
