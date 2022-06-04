a1b4a2dc4a3f28605f809e1048962d08dc5c1bc3
Ariawhan <gede.aryawhan@gmail.com>
2022-06-04 19:47:15 +0800

    [Ariawan] Adding EmailAlreadyTakenError.js to repository

---
+++ app/errors/EmailAlreadyTakenError.js
@@ -0,0 +1,14 @@
+const ApplicationError = require('./ApplicationError');
+
+class EmailAlreadyTakenError extends ApplicationError {
+  constructor(email) {
+    super(`${email} is already taken!!!`);
+    this.email = email;
+  }
+
+  get details() {
+    return { email: this.email };
+  }
+}
+
+module.exports = EmailAlreadyTakenError;
