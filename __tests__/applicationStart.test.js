48b1edec6a6d19d35960492bd02ae00d9be92047
Ariawhan <gede.aryawhan@gmail.com>
2022-06-10 02:54:49 +0800

    [Ariawan] Update integration test applications start cars with jest

--- __tests__/applicationStart.test.js
+++ __tests__/applicationStart.test.js
@@ -11,4 +11,19 @@ describe("GET /", () => {
         expect(res.body.message).toEqual("BCR API is up and running!");
       });
   });
+  it("should response with 404 as status code (Application Not found)", async () => {
+    return request(app)
+      .get("/Ap")
+      .then((res) => {
+        console.log(res.body);
+        expect(res.statusCode).toBe(404); // check toBe 200 status response sukses
+        expect(res.body).toEqual({
+          error: {
+            name: "Error",
+            message: "Not found!",
+            details: { method: "GET", url: "/Ap" },
+          },
+        });
+      });
+  });
 });
